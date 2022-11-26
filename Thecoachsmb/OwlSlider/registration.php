01f0673c42cae185a22c404794bd912cbe6fa747
shrutienno <shruti@ennomail.com>
2022-11-25 19:07:46 -0800

    owl-caraousel

---
+++ Thecoachsmb/OwlSlider/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created By : TheCoachSMB - Sonal Motghare-Balpande
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Thecoachsmb_OwlSlider',
+    __DIR__
+);
---
+++ Thecoachsmb/OwlSlider/view/frontend/requirejs-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created By : TheCoachSMB - Sonal Motghare-Balpande
+ */
+var config = {
+    paths: {
+        owlcarousel: "Thecoachsmb_OwlSlider/js/owl.carousel"
+    },
+    shim: {
+        owlcarousel: {
+            deps: ['jquery']
+        }
+    }
+};
